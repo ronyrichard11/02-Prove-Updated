86fe23759c4da51e5dc5d717a25ca80b3367abb7
ronyrichard11 <83197344+ronyrichard11@users.noreply.github.com>
2022-06-18 21:13:38 -0600

    Add files via upload

---
+++ Final/Program.cs
@@ -0,0 +1,72 @@
+﻿/*
+Higher Lower Game Program
+Rony Richard
+*/
+using System;
+
+namespace Program
+{
+    class Program //Sorry Only One Class. I managed to make the Point System Run based on whenever the Program Restarted So I failed to find a need for a Second Class.
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool Restart = true;
+            int min = 1;
+            int max = 13;
+            int guess;
+            int number;
+            int points;
+            String response;
+
+            while (Restart)
+            {
+                guess = 0;
+                points = 300;
+                response = "";
+                number = random.Next(min, max + 1);
+
+                while (guess != number)
+                {
+                    Console.WriteLine("Guess a number between " + min + " - " + max + " : ");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Guess: " + guess);
+
+                    if (guess > number)
+                    {
+                        Console.WriteLine(guess + " is to high! Try Again.");
+                        points -= 75;
+
+                    }
+                    else if (guess < number)
+                    {
+                        Console.WriteLine(guess + " is to low! Try Again");
+                        points -= 75;
+                    }
+                    //Basically for my point tracking. Every time they loop through one of the two "Try Again" Messages 75 points will be deducted. Everytime they get past the loop, 100 points will be added.
+                }
+                Console.WriteLine("Number: " + number);
+                Console.WriteLine("Congratulations, You guessed the Number Properly!");
+                points += 100;
+                Console.WriteLine("Points: " + points);
+
+                Console.WriteLine("Play Again? (Y/N): ");
+                response = Console.ReadLine();
+                response = response.ToUpper();
+
+                if (response == "Y")
+                {
+                    Restart = true;
+                }
+                else
+                {
+                    Restart = false;
+                }
+            }
+
+            Console.WriteLine("Thanks for playing!");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Final/README.txt
@@ -0,0 +1,5 @@
+Hello! This is my README.txt file for my Higher Lower Program.
+
+Any specific functions in my Higher Lower program have a comment attached to them in order for it to be understood.
+I spent roughly 4 days figuring it out myself and then got stuck on a local function cannot contain reference problem. If i managed to fix that I would have had it running smoothly and everything
+would loop into eachother. I couldnt figure it out so I watched a few youtube tutorials on ref and changed my approach and now this is my finished product.
